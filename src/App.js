e422e66fa3578461d4e64d66fb55790d43a6e94d
Rama Devi <91324864+rdg0317@users.noreply.github.com>
2023-05-19 17:49:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Navbar from './components/navbar';
+import About from './components/about';
+import Footer from './components/footer';
+
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar></Navbar>
+        <section><About/></section>
+        <Footer></Footer>   
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './stylesheets/styles.css';
+import signup_form from './components/alumni_signup';
+function Navbar(){
+   return(
+    <div className="nav">
+     <img className="logo" src="src/image.jpg"></img>
+        <div className="nav-bar">
+            <ul className="nav-list">
+                    <li className="nav-item">Home</li>
+                    <li className="nav-item">Alumni</li>
+                    <li className="nav-item">Opportunities</li>
+                    <li className="nav-item">News/Events</li>
+                    <li className="nav-item">Contact Us</li>
+            </ul>
+             <div className="login-signup" onClick={<signup_form></signup_form>}></div>
+         </div>
+     </div>
+   );
+}
+
+export default Navbar
