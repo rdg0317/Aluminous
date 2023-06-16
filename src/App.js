01d76cc7f63e524d7764c74ee249f41c74e58ebb
Rama Devi <91324864+rdg0317@users.noreply.github.com>
2023-06-16 23:16:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-
-import Navbar from './components/navbar';
-import About from './components/about';
-import Footer from './components/footer';
-
-
-function App() {
-  return (
-    <div className="App">
-       <Navbar></Navbar>
-        <section><About/></section>
-        <Footer></Footer>   
-        
-    </div>
-  );
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/css/styles.css'
+import NavBar from "./components/inc/Navbar";
+import  Carousel  from "./components/inc/Carousel";
+import Content from "./components/inc/Content";
+import Footer from "./components/inc/Footer";
+import Form from "./components/inc/Form";
+function App(){
+    return(
+<div position="relative">
+<NavBar/>
+<Carousel/>
+    <Content></Content>
+<Footer/>
+</div>
+    );
 }
 
 export default App;
